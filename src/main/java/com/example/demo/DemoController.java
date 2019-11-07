50407a2aeaca9a7bb116d6859df332a1ba4862fa
Mikhail Usatenko <usatenkomihailh@gmail.com>
2019-11-07 16:40:32 +0200

    1.Change port from 8080 to 8081 2.Add controller
    Signed-off-by: Mikhail Usatenko <usatenkomihailh@gmail.com>

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello")
+public class DemoController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+}
